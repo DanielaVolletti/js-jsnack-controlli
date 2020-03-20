50fb90ca501d10e391095be33b7de584042b5b83
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-20 15:40:27 +0100

    esercizio due

---
+++ esercizio-due/js/main.js
@@ -0,0 +1,22 @@
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+// inserimento prima parola utente
+var parolaUno = prompt("Inserisci una parola:");
+
+// inserimento seconda parola utente
+var parolaDue = prompt("Inserisci un'altra parola:");
+
+// var lunghezza parola
+var parolaCorta;
+
+// stampa prima parola più lunga, poi più corta
+if (parolaUno.length < parolaDue.length) {
+  parolaCorta = parolaUno + " , " + parolaDue;
+} else if (parolaDue.length < parolaUno.length){
+  parolaCorta = parolaDue + " , " + parolaUno;
+}
+
+console.log(parolaCorta);
+
+// output stampa
+document.getElementById('stampa-in-sequenza').innerHTML = "In ordine di lunghezza di parole, le parole inserite sono: " + parolaCorta;
